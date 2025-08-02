199b5e1e83532ee377c6c27bb0ae9b6ed4e41a47
YASH SHAHA <yashb1117@gmail.com>
2025-08-02 13:41:52 +0530

    Update main.tf

--- Terra2/main.tf
+++ Terra2/main.tf
@@ -50,7 +50,8 @@ resource "aws_instance" "TF2EC2" {
   key_name= "git"
   subnet_id =aws_subnet.YS1.id
   availability_zone = "us-east-1a"
-  security_groups = "New same vpc"
+  security_groups = ["New same vpc"]
+
 root_block_device {
     volume_size = 30
     volume_type = "gp2"
